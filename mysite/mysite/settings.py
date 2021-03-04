5f8e8f7467be43dbfa7a2e72afb8b13611e5bead
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-03-04 21:48:56 +0530

    Settings updated for User app entry, Custom User Model modifications

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'user.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
