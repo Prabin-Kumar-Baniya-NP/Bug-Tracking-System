3a81c6094d749062578d5bb72bbd0adfafad4282
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-03-04 21:51:28 +0530

    Custom User Model created inheriting AbstractUser

--- mysite/user/models.py
+++ mysite/user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    about_me = models.TextField("About me", max_length=500, blank=True)
+    address = models.CharField("Address", max_length=30, blank=True)
+    birth_date = models.DateField("Date of birth", null=True, blank=True)
