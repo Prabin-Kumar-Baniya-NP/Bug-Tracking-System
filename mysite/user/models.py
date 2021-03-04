6f3ea0f287b03fe4d3ba475c4ba698bd9996fee8
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-03-04 23:03:16 +0530

    Extra fields added into User Model

--- mysite/user/models.py
+++ mysite/user/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from company.models import Company
+from product.models import Product
+from team.models import Team
+from designation.models import Designation
 
 class User(AbstractUser):
     about_me = models.TextField("About me", max_length=500, blank=True)
     address = models.CharField("Address", max_length=30, blank=True)
     birth_date = models.DateField("Date of birth", null=True, blank=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null = True, blank = True)
+    product = models.ManyToManyField(Product, blank = True)
+    team = models.ManyToManyField(Team, blank = True)
+    designation = models.ManyToManyField(Designation, blank = True)
