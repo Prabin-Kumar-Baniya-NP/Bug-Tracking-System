0a084e421dea18a324266f0bee3e31d97eb4658c
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-03-07 18:14:05 +0530

    Modified for adding administrator field

--- mysite/designation/models.py
+++ mysite/designation/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
 
 class Designation(models.Model):
     name = models.CharField(max_length = 50)
+    administrator = models.ManyToManyField(settings.AUTH_USER_MODEL)
