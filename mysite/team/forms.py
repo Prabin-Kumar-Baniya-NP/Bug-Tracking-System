c10628489e52756ed6abd7f6ad8d8c5d9c411f19
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-03-18 22:40:51 +0530

    Product field added into Team

--- mysite/team/forms.py
+++ mysite/team/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from team.models import Team
 from django.forms import ModelForm
+from product.models import Product
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -11,4 +12,5 @@ class TeamCreationForm(ModelForm):
     
     def __init__(self, requested_user_id, *args, **kwargs):
         super(TeamCreationForm, self).__init__(*args, **kwargs)
+        self.fields["product"].queryset = Product.objects.filter(administrator = requested_user_id)
         self.fields['administrator'].queryset = User.objects.filter(id = requested_user_id)
--- mysite/team/models.py
+++ mysite/team/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
+from product.models import Product
 
 class Team(models.Model):
     name = models.CharField(max_length = 50)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null = True)
     administrator = models.ManyToManyField(settings.AUTH_USER_MODEL)
