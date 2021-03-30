0db0159274a241357a27174cf95e8ab92a818804
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-03-30 12:09:48 +0530

    Object name added

--- mysite/team/models.py
+++ mysite/team/models.py
@@ -6,3 +6,6 @@ class Team(models.Model):
     name = models.CharField(max_length = 50)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null = True)
     administrator = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.name + " - " + self.product.name
