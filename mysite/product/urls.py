739249de16c5b18cd7d629d7ddaa2b4163e87734
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-03-30 08:58:17 +0530

    Product Updation and Deletion Features added

--- mysite/product/urls.py
+++ mysite/product/urls.py
@@ -4,4 +4,8 @@ from product import views
 app_name = "product"
 urlpatterns = [
     path("product-creation-page", views.productCreationView, name = "product-creation"),
+    path("product-updation-page/<int:product_id>", views.productUpdationView, name = "product-updation"),
+    path("product-deletion-page/<int:product_id>", views.productDeletionView, name = "product-deletion"),
+    path("", views.ProductListView.as_view(), name = "product-list"),
+    path("<pk>/", views.ProductDetailView.as_view(), name= "product-details"),
 ]
--- mysite/product/views.py
+++ mysite/product/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, reverse
-from product.forms import ProductCreationForm
+from product.forms import ProductCreationForm, ProductUpdationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-
+from django.views import generic 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from product.models import Product
 
 @login_required
 def productCreationView(request):
@@ -18,3 +20,51 @@ def productCreationView(request):
             'form': ProductCreationForm(request.user.id),
         }
         return render(request,  "product/product-creation-page.html", context)
+
+class ProductListView(LoginRequiredMixin, generic.ListView):
+    model = Product
+    template_name = "product/index.html"
+
+    def get_queryset(self):
+        return Product.objects.filter(administrator = self.request.user.id)
+
+class ProductDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Product
+    template_name = "product/details.html"
+
+@login_required
+def productUpdationView(request, product_id):
+    requested_product = Product.objects.get(id = product_id)
+    if request.user in requested_product.administrator.all():
+        if request.method == "POST":
+            form = ProductUpdationForm(product_id, request.POST, instance = requested_product)
+            if request.user in requested_product.administrator.all():
+                if form.is_valid():
+                    form.save()
+                    messages.success(request, "Product Details Updated Successfully")
+                    return HttpResponseRedirect(reverse("user:dashboard"))
+                else:
+                    messages.error(request, "Please enter valid data")
+                    return HttpResponseRedirect(reverse("user:dashboard"))
+            else:
+                messages.error(request, "You dont have permissions to update the company details")
+                return HttpResponseRedirect(reverse("user:dashboard"))
+        else:
+            context = {
+                'form': ProductUpdationForm(product_id, instance = requested_product),
+            }
+            return render(request,"product/product-updation-page.html", context)
+    else:
+        messages.error(request, "You are not eligible to update the product details")
+        return HttpResponseRedirect(reverse("user:dashboard"))
+
+@login_required
+def productDeletionView(request, product_id):
+    requested_product = Product.objects.get(id = product_id)
+    if request.user in requested_product.administrator.all():
+        requested_product.delete()
+        messages.success(request, "Product deleted successfully")
+        return HttpResponseRedirect(reverse("user:dashboard"))
+    else:
+        messages.error(request, "You are not eligible to delete the company details")
+        return HttpResponseRedirect(reverse("user:dashboard"))
