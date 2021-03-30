3a6e5ca22770d754007fcf8dfdd73d698d4873f1
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-03-30 12:09:34 +0530

    Object name added

--- mysite/company/models.py
+++ mysite/company/models.py
@@ -4,4 +4,7 @@ from django.conf import settings
 class Company(models.Model):
     name = models.CharField(max_length = 50)
     description = models.TextField(max_length=500, null = True, blank = True)
-    administrator = models.ManyToManyField(settings.AUTH_USER_MODEL)    
+    administrator = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.name
