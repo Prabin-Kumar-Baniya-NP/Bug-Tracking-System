478b201186b5891eda0a4d95a3178e69ad6de9d6
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-04-02 11:49:56 +0530

    Urls path updated for ticket update and review

--- mysite/ticket/urls.py
+++ mysite/ticket/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("report-bug/", views.report_bug, name = "report-bug"),
     path("submitted-tickets/", views.SubmittedTicketsListView.as_view(), name = "submitted-tickets"),
     path("review-tickets/", views.reviewTickets, name = "review-tickets"),
+    path("ticket-details/<int:pk>", views.TicketDetailView.as_view(), name= "ticket-details"),
+    path("ticket-updation/<int:ticket_id>", views.ticketUpdation, name = "update-ticket"),
 ]
