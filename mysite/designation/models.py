5f76c573794b2421bd89c9a9947021b8dfb8893b
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-04-04 19:33:58 +0530

    Extra fields added into Team and Designation models

--- mysite/designation/models.py
+++ mysite/designation/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.conf import settings
 from team.models import Team
-
+from django.utils import timezone
 class Designation(models.Model):
     name = models.CharField(max_length = 50)
+    description = models.TextField(max_length=500, default="Designation Description")
     team = models.ForeignKey(Team, on_delete=models.CASCADE, null = True)
     administrator = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    created_on = models.DateTimeField(default=timezone.now, editable = False)
 
     def __str__(self):
         return self.name + " - " + self.team.name
--- mysite/team/models.py
+++ mysite/team/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.conf import settings
 from product.models import Product
+from django.utils import timezone
 
 class Team(models.Model):
     name = models.CharField(max_length = 50)
+    description = models.TextField(max_length=500, default="Team Description")
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null = True)
     administrator = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    created_on = models.DateTimeField(default=timezone.now, editable = False)
 
     def __str__(self):
         return self.name + " - " + self.product.name
