a95ade7616311b1ae1d77c154c993c156f0ab19a
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-04-05 20:13:55 +0530

    dj_database module used for postgres config

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.parse(config("DATABASE_URL"), conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
