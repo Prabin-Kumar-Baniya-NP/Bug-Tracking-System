45e01e0cf1ff1cb02083336e6915bca00c9c37af
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-04-05 17:39:47 +0530

    heroku domain added to allowed host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["bug-tracking-system.herokuapp.com", "127.0.0.1"]
 
 
 
