4b69d93f0c234204f50cabf64d6bee9f2afbcd13
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-04-15 08:04:17 +0530

    Added support for sqlite in local environment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,12 +83,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
+if DEBUG == True:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2'
+        }
+    }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
