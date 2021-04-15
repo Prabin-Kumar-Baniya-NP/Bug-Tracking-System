0005d0e4af1542f3cede504d182922b243ca1bee
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-04-15 08:07:35 +0530

    Added support for sqlite in local environment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -96,9 +96,8 @@ else:
             'ENGINE': 'django.db.backends.postgresql_psycopg2'
         }
     }
-
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+    db_from_env = dj_database_url.config(conn_max_age=600)
+    DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
